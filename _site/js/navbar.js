566899c66cd2d02e58d5e3d607b1a542fecace9a
Tom Janssens <tom@timble.net>
2014-05-15 17:04:54 +0200

    Added _site directory

---
+++ _site/js/navbar.js
@@ -0,0 +1,12 @@
+/*
+ Inspired by Flaunt.js - https://github.com/toddmotto/flaunt-js
+ */
+;(function($) {
+    // DOM ready
+    $(function() {
+        // Click to show the navigation
+        $('.navbar__handle').click(function(){
+            $('.navbar .nav').toggleClass('is-shown');
+        });
+    });
+})(jQuery);
